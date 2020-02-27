8aba603844bd2c8f323f37e0d03fe2ee95f33e5b
LuisEduardoPM <44383776+LuisEduardoPM@users.noreply.github.com>
2020-02-27 12:50:46 -0600

    Add files via upload

---
+++ pgskalu.py
@@ -0,0 +1,81 @@
+#Libreria para llamar al sistema operativo
+import os
+#Definimos los octetos de la red
+red = "200.33.171.0/24"
+os.system("nmap -sP " + red)
+
+C:\Users\Luis\Desktop\ITM>python pgskalu.py
+Starting Nmap 7.80 ( https://nmap.org ) at 2020-02-27 12:37 Hora estßndar central (MÚxico)
+Failed to resolve "200.33.171.".
+WARNING: No targets were specified, so 0 hosts scanned.
+Nmap done: 0 IP addresses (0 hosts up) scanned in 0.47 seconds
+
+C:\Users\Luis\Desktop\ITM>python pgskalu.py
+Starting Nmap 7.80 ( https://nmap.org ) at 2020-02-27 12:38 Hora estßndar central (MÚxico)
+Stats: 0:02:18 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 91.85% done; ETC: 12:40 (0:00:12 remaining)
+Stats: 0:02:18 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 91.89% done; ETC: 12:40 (0:00:12 remaining)
+Stats: 0:02:20 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.09% done; ETC: 12:40 (0:00:12 remaining)
+Stats: 0:02:21 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.28% done; ETC: 12:40 (0:00:12 remaining)
+Stats: 0:02:24 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.63% done; ETC: 12:41 (0:00:11 remaining)
+Stats: 0:02:25 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.68% done; ETC: 12:41 (0:00:11 remaining)
+Stats: 0:02:25 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.77% done; ETC: 12:41 (0:00:11 remaining)
+Stats: 0:02:27 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 92.92% done; ETC: 12:41 (0:00:11 remaining)
+Stats: 0:02:28 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 93.02% done; ETC: 12:41 (0:00:11 remaining)
+Stats: 0:02:28 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 93.46% done; ETC: 12:41 (0:00:10 remaining)
+Stats: 0:02:29 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 93.70% done; ETC: 12:41 (0:00:10 remaining)
+Stats: 0:02:30 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 93.75% done; ETC: 12:41 (0:00:10 remaining)
+Stats: 0:03:14 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 99.80% done; ETC: 12:41 (0:00:00 remaining)
+Stats: 0:03:15 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 99.85% done; ETC: 12:41 (0:00:00 remaining)
+Stats: 0:03:24 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 99.90% done; ETC: 12:41 (0:00:00 remaining)
+Stats: 0:03:25 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Ping Scan Timing: About 100.00% done; ETC: 12:41 (0:00:00 remaining)
+Stats: 0:03:27 elapsed; 0 hosts completed (0 up), 256 undergoing Ping Scan
+Parallel DNS resolution of 256 hosts. Timing: About 46.67% done; ETC: 12:41 (0:00:01 remaining)
+Nmap scan report for delfin2.itmorelia.edu.mx (200.33.171.11)
+Host is up (0.011s latency).
+Nmap scan report for 200.33.171.13
+Host is up (0.0064s latency).
+Nmap scan report for dsc.itmorelia.edu.mx (200.33.171.20)
+Host is up (0.00013s latency).
+Nmap scan report for libra.itmorelia.edu.mx (200.33.171.54)
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.66
+Host is up (0.024s latency).
+Nmap scan report for denebvirtual.itmorelia.edu.mx (200.33.171.77)
+Host is up (0.0020s latency).
+Nmap scan report for sappp.itmorelia.edu.mx (200.33.171.80)
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.86
+Host is up (0.0040s latency).
+Nmap scan report for 200.33.171.99
+Host is up (0.00s latency).
+Nmap scan report for 200.33.171.115
+Host is up (0.0020s latency).
+Nmap scan report for 200.33.171.120
+Host is up (0.0020s latency).
+Nmap scan report for vinculacion.itmorelia.edu.mx (200.33.171.124)
+Host is up (0.0040s latency).
+Nmap scan report for 200.33.171.125
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.127
+Host is up (0.0010s latency).
+Nmap scan report for 200.33.171.254
+Host is up (0.0010s latency).
+Nmap done: 256 IP addresses (15 hosts up) scanned in 211.66 seconds
+
+C:\Users\Luis\Desktop\ITM>
